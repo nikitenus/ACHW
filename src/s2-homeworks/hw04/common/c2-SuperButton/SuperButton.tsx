b3d3f573fab1779d71e9651aaff7a281b501eca6
Vladislav Nikitenkov <v.nikitenkov@Vladislavs-MacBook-Pro.local>
2022-12-28 17:31:18 +0300

    hw4

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -17,12 +17,7 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
         ...restProps // все остальные пропсы попадут в объект restProps, там же будет children
     }
 ) => {
-    const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+    const finalClassName = `${s.button} ${xType ? s.red : s.default} ${className}`
 
     return (
         <button
